39b5631f4605285b4d9c1b2c4e3464b842128df4
Atsuko Ito <me@yottatsa.name>
2022-06-30 13:43:15 +0100

    debug part 2
    * `_serial_putch` and `_serial_puts` macroses are reusable and working
    * checksum16
    * testing memory by bytes and by words

---
+++ fw/checksum.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+import struct
+import sys
+with open(sys.argv[1], "rb+") as f:
+    f.seek(0x3fd00)
+    checksum = 0
+    i = 0
+    while f.peek(2):
+        i = i + 1
+        checksum = (checksum + struct.unpack('<H', f.read(2))[0]) % 0x10000
+    f.seek(0x3fc00)
+    print(i, hex(checksum))
+    f.write(struct.pack('<H', checksum))
