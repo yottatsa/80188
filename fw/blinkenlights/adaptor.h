1d24533d7bffb0b80182b7041dc9dbcf366e88ee
Atsuko Ito <me@yottatsa.name>
2022-09-18 00:53:25 +0100

    blinkenlights initial integration
    Using https://justine.lol/blinkenlights/ to emulate BIOS ROM
    
     * imported https://github.com/jart/cosmopolitan
     * quick-and-dirty build using amalgamated libc

---
+++ fw/blinkenlights/adaptor.h
@@ -0,0 +1,8 @@
+#define _getttysize getttysize
+#define _tpenc tpenc
+#define _startswith startswith
+#define _bsrl bsrl
+#define _bsfl bsfl
+#define _rounddown2pow rounddown2pow
+#define _bsr bsr
+#include "libc/macros.internal.h"
---
+++ fw/blinkenlights/cosmopolitan/libc/thread/tls.h
@@ -0,0 +1,14 @@
+#ifndef COSMOPOLITAN_LIBC_THREAD_TLS_H_
+#define COSMOPOLITAN_LIBC_THREAD_TLS_H_
+/*
+#include "libc/thread/thread.h"
+*/
+
+#define TLS_ALIGNMENT 64
+
+#if !(__ASSEMBLER__ + __LINKER__ + 0)
+COSMOPOLITAN_C_START_
+
+COSMOPOLITAN_C_END_
+#endif /* !(__ASSEMBLER__ + __LINKER__ + 0) */
+#endif /* COSMOPOLITAN_LIBC_THREAD_TLS_H_ */
